d5cad6aaf67586695eebbbd786830ff40ea319c3
Abil Momin <108158166+Astroboy1011@users.noreply.github.com>
2022-06-25 16:19:47 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Task 1:
+        /*
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        */
+
+        // Task 2:
+        /*
+        for (char i = 65; i <= 67; i++) {
+            for (char j = 65; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+         */
+
+        // Task 3:
+        /*
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4; j++) {
+                if (i == 1 || i == 4 || j == 1 || j == 4)
+                    System.out.print("$ ");
+                else
+                System.out.print("  ");
+            }
+            System.out.println();
+        }
+         */
+
+
+    }
+}
